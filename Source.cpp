87582b4372d3b8aef4de36aacceaa1632fb5f094
cecoIryna <fashionableirina2018@gmail.com>
2024-09-23 10:15:59 +0300

    new

--- Source.cpp
+++ Source.cpp
@@ -4,15 +4,18 @@ using namespace std;
 class CPU
 {
 	char* model;
+	int cores;
 public:
-	CPU(const char* m)
+	CPU(const char* m, int c)
 	{
 		model = new char[strlen(m) + 1];
 		strcpy_s(model, strlen(m) + 1, m);
+		cores = c;
 	}
 	void PrintCPU()
 	{
 		cout << "CPU model: " << model << endl;
+		cout << "CPU cores number: " << cores << endl;
 	}
 	~CPU()
 	{
@@ -21,66 +24,104 @@ public:
 };
 class SDD
 {
-	char* model;
+	char* brand;
+	int capacity;
 public:
-	SDD(const char* m)
+	SDD(const char* b, int cap)
 	{
-		model = new char[strlen(m) + 1];
-		strcpy_s(model, strlen(m) + 1, m);
+		brand = new char[strlen(b) + 1];
+		strcpy_s(brand, strlen(b) + 1, b);
+		capacity = cap;
 	}
-	void PrintCond()
+	void PrintSDD()
 	{
-		cout << "SDD model: " << model << endl;
+		cout << "SDD brand: " << brand << endl;
+		cout << "SDD capacity: " << capacity << endl;
 	}
 	~SDD()
 	{
-		delete[]model;
+		delete[]brand;
 	}
 };
-class SDD
+class VideoCard
 {
 	char* model;
+	int memory;
 public:
-	SDD(const char* m)
+	VideoCard(const char* m, int mem)
 	{
 		model = new char[strlen(m) + 1];
 		strcpy_s(model, strlen(m) + 1, m);
+		memory = mem;
 	}
-	void PrintCond()
+	void PrintVideoCard()
 	{
-		cout << "SDD model: " << model << endl;
+		cout << "Video Card: " << model << endl;
+		cout << "Memory of Video Card: " << memory << " GB" << endl;
 	}
-	~SDD()
+	~VideoCard()
 	{
 		delete[]model;
 	}
 };
-class Car
+class RAM
 {
-	char* model;
-	const int year;
-	Engine eng;
-	Cond c;
+	char* type;
+	int memory;
 public:
-	Car(const char* n, int y, const char* eng_name, const char* c_name) : year(y), eng(eng_name), c(c_name)
+	RAM(const char* t, int mem)
 	{
-		model = new char[strlen(n) + 1];
-		strcpy_s(model, strlen(n) + 1, n);
+		type = new char[strlen(t) + 1];
+		strcpy_s(type, strlen(t) + 1, t);
+		memory = mem;
 	}
-	~Car()
+	void PrintRAM()
 	{
-		delete[]model;
+		cout << "Video Card: " << type << endl;
+		cout << "Memory of Video Card: " << memory << " GB" << endl;
 	}
-	void PrintCar() {
-		cout << model << endl;
-		cout << year << endl;
-		eng.PrintEngine();
-		c.PrintCond();
+	~RAM()
+	{
+		delete[]type;
 	}
 };
+class Laptop {
+	int count = 0;
+	char* brand;
+	char* color;
+	int price;
+	CPU cpu;
+	SDD sdd;
+	VideoCard videoCard;
+	RAM ram;
+public:
+	Laptop(const char* b, const char* col, int p, const char* cpu_m, int cpu_c, const char* sdd_b, int sdd_cap, const char* videoCard_m, int videoCard_mem, const char* ram_t, int ram_mem): price(p), cpu(cpu_m, cpu_c), sdd(sdd_b, sdd_cap), videoCard(videoCard_m, videoCard_mem), ram(ram_t, ram_mem)
+	{
+		brand = new char[strlen(b) + 1];
+		strcpy_s(brand, strlen(b) + 1, b);
+		color = new char[strlen(col) + 1];
+		strcpy_s(color, strlen(col) + 1, col);
+		count++;
+	}
+
+	~Laptop() {
+		delete[] brand;
+		delete[] color;
+		count--;
+	}
+
+	void PrintLaptop() const {
+		cout << "Laptop brand: " << brand << endl;
+		cout << "Price: " << price << "$" << endl;
+		cout << "Color : " << color << endl;
+		cpu.PrintCPU();
+		sdd.PrintSDD();
+		videoCard.PrintVideoCard();
+		ram.PrintRAM();
+	}
 
 int main()
 {
-	Car car1("Toyota", 2003, "V8 Engine", "Bosh");
-	car1.PrintCar();
+	Laptop laptop1("Dell", "Black", 1200, "Intel i7", 8, "Samsung", 512, "NVIDIA GTX 1650", 4, "DDR4", 16);
+	laptop1.PrintLaptop();
 }
